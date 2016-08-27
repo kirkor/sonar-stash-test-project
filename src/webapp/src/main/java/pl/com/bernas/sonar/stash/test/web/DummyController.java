03a319d406d58532b6ea39581235aa794607ec7a
Grzegorz Bernas <grzegorz@bernas.com.pl>
2016-08-27 13:39:59 +0200

    initial commit for web module

---
+++ src/webapp/src/main/java/pl/com/bernas/sonar/stash/test/web/DummyController.java
@@ -0,0 +1,14 @@
+package pl.com.bernas.sonar.stash.test.web;
+
+/**
+ * Created by kirkor on 8/27/2016.
+ */
+public class DummyController {
+    public String helloWorld() {
+        String unused = "Bla bla bla";
+
+        String duplication = "Bla bla bla";
+
+        return "Bla bla bla";
+    }
+}
